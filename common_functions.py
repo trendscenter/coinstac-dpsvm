79746cd93d170f9d9e4e8fe5e1de487fded2f9d8
hvgazula <hvgazula@umich.edu>
2018-06-06 10:12:49 -0600

    All good except common_functions.py

--- common_functions.py
+++ common_functions.py
@@ -1,10 +1,8 @@
-from scipy import array, sign, dot
+from scipy import sign, dot
 from matplotlib.pylab import demean
 
 
 def test_errors(ws, data, labels):
-    #    error = {}
-    #    for v in ws:
     error = 100 * abs(
         sum(map(lambda x: min(0, x), sign(
             dot(ws, data.T)) * labels))) / labels.shape[0]
@@ -15,7 +13,6 @@ def data2data(data, clfs):
     """
     Applies all classifiers to the data and returns their output as vectors
     """
-    W = array([clfs[i] for i in range(0, len(clfs))])
-    X = dot(data, W.T)
+    X = dot(data, clfs)
     X = demean(X, axis=1)
     return X
--- remote.py
+++ remote.py
@@ -1,4 +1,3 @@
-import numpy as np
 import os
 import pickle
 from sklearn.linear_model import LogisticRegression
@@ -15,47 +14,26 @@ def remote_1(args):
 
     with open(os.path.join(args["state"]["baseDirectory"], train_file_name),
               'rb') as ftrain:
-        train_data = pickle.load(ftrain)
+        X_train, y_train = pickle.load(ftrain)
 
     with open(os.path.join(args["state"]["baseDirectory"], test_file_name),
               'rb') as ftest:
-        test_data = pickle.load(ftest)
-
-    W_site = [input_list[site]["W_site"] for site in input_list]
+        X_test, y_test = pickle.load(ftest)
 
     # train (use public logistic regression classifer to aggregate
     # svm weights from each site)
-    sols = []
-    train_data_mapped = common_functions.data2data(train_data[0], sols)
-    clf = LogisticRegression()
-    clf.fit(train_data_mapped, train_data[1])
+    W_site = [input_list[site]['W_site'] for site in input_list]
 
-    raise Exception('hi there')
+    train_data_mapped = common_functions.data2data(X_train, W_site)
+    clf = LogisticRegression()
+    clf.fit(train_data_mapped, y_train)
 
     # test
-    test_data_mapped = common_functions.data2data(test_data[0], sols)
+    test_data_mapped = common_functions.data2data(X_test, W_site)
     e = 100 * abs(
         sum(
             map(lambda x: min(0, x),
-                clf.predict(test_data_mapped) * test_data[1]))) / float(
-                    len(test_data[1]))
-
-#    # also generate the error rate for test_data based on the W_site
-#    e_site = []
-#    for i in range(0, 4):
-#        e_site.append(
-#            common_functions.test_errors(
-#                np.asarray(sols[i]),
-#                np.asarray(test_data[0]), np.asarray(test_data[1])))
-#
-#    sys.stderr.write(
-#        "final error rate of test data using aggregated classifier is: " +
-#        str(e) + "\n")
-#    sys.stderr.write(
-#        "error rates of test data using SVM weights from each site are: ")
-#    sys.stderr.write(
-#        str(e_site[0]) + ',' + str(e_site[1]) + ',' + str(e_site[2]) + ',' +
-#        str(e_site[3]))
+                clf.predict(test_data_mapped) * y_test))) / len(y_test)
 
     output_dict = {{"final_error_rate": e}}
 
